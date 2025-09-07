332294ba3d258897350a81f76efad8801b8b7d4b
Yazor <yazor@herobrine.fr>
2025-09-07 14:59:22 -0400

    init project

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const CopywebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    app: './src/scripts/app.ts'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      }, {
+        test: /\.(png|gif|jpg|jpeg|svg|xml)$/,
+        use: [ 'url-loader' ]
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist')
+  }
+};
